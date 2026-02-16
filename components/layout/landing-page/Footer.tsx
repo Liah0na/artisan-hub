7292a6170ebff7b312bef223764f7ae353626f7c
Nestor Otondo <notondo@byupathway.edu>
2026-02-16 19:05:03 -0300

    Adding footer styles

--- components/layout/landing-page/Footer.tsx
+++ components/layout/landing-page/Footer.tsx
@@ -6,11 +6,15 @@ const Footer = () => {
     <footer className="border-t border-primary py-8">
       <Container>
         <div className="grid gap-4 grid-cols-3">
-          <Image src="/logo.png" alt="Artisan Hub Logo" width={122} height={27} />
+          <section>
+            <Image className="pb-3" src="/logo.png" alt="Artisan Hub Logo" width={122} height={27} />
+            <p className="font-secondary text-lg">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed consectetur necessitatibus itaque repellat adipisci fuga modi debitis!.</p>
+          </section>
           <ul>
-            <li><a href="#" className="mx-2 text-gray-400 hover:text-white transition-colors duration-300">Política de Privacidad</a></li>
-            <li><a href="#" className="mx-2 text-gray-400 hover:text-white transition-colors duration-300">Términos de Servicio</a></li>
-            <li><a href="#" className="mx-2 text-gray-400 hover:text-white transition-colors duration-300">Contacto</a></li>
+            <li><a href="#" className="mx-2 text-gray-400 hover:hover-primary transition-colors duration-300">Acerca de nosotros</a></li>
+            <li><a href="#" className="mx-2 text-gray-400 hover:hover-primary transition-colors duration-300">Política de Privacidad</a></li>
+            <li><a href="#" className="mx-2 text-gray-400 hover:hover-primary transition-colors duration-300">Términos de Servicio</a></li>
+            <li><a href="#" className="mx-2 text-gray-400 hover:hover-primary transition-colors duration-300">Contacto</a></li>
           </ul>
           <div>
             <p className="text-sm">&copy; 2026 ArtisanHub. Todos los derechos reservados.</p>

92571fc250cf3e23cc036f53251c013e47c73c74
Nestor Otondo <notondo@byupathway.edu>
2026-03-24 16:31:49 -0300

    Chanching page props params

--- app/artisan/[id]/page.tsx
+++ app/artisan/[id]/page.tsx
@@ -1,16 +1,17 @@
-import { notFound } from "next/navigation";
-import ArtisanView from "@/components/layout/artisan/ArtisanView";
-import { getArtisanById } from "@/lib/services/artisan.service";
+import { notFound } from 'next/navigation';
+import ArtisanView from '@/components/layout/artisan/ArtisanView';
+import { getArtisanById } from '@/lib/services/artisan.service';
 
-interface PageProps {
-  params: Promise<{ id: string }>;
-}
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
 
 export default async function ArtisanPage({ params }: PageProps) {
-  const { id } = await params;
-  const artisan = await getArtisanById(id);
+  const artisan = await getArtisanById(params.id);
 
   if (!artisan) return notFound();
 
-  return <ArtisanView id={id} />
+  return <ArtisanView id={params.id} />
 }

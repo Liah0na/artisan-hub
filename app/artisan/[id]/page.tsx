6764c0aae250f924e27a879de039bc2dbb056369
Nestor Otondo <notondo@byupathway.edu>
2026-03-24 22:50:43 -0300

    Fixing promises params id

--- app/artisan/[id]/page.tsx
+++ app/artisan/[id]/page.tsx
@@ -3,15 +3,13 @@ import ArtisanView from '@/components/layout/artisan/ArtisanView';
 import { getArtisanById } from '@/lib/services/artisan.service';
 
 type PageProps = {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 };
 
 export default async function ArtisanPage({ params }: PageProps) {
-  const artisan = await getArtisanById(params.id);
+  const artisan = await getArtisanById((await params).id);
 
   if (!artisan) return notFound();
 
-  return <ArtisanView id={params.id} />
+  return <ArtisanView id={(await params).id} />
 }
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -3,13 +3,11 @@ import ProductView from '@/components/layout/product/ProductView';
 import { getProductById } from '@/lib/services/product.service';
 
 type PageProps = {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 };
 
 export default async function ProductPage({ params }: PageProps) {
-  const product = await getProductById(params.id);
+  const product = await getProductById((await params).id);
 
   if (!product) notFound();
 
